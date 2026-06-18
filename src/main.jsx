a58a925c2001051b06e54d2377481ddd313fe3d7
mohammad mahdi ghorbani <mohammadmahdi271uni@gmail.com>
2026-06-18 14:32:48 +0330

    build: configure Vite, HashRouter, and deploy scripts for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -6,8 +6,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
      <StrictMode>
-          <BrowserRouter>
+          <HashRouter>
                <App />
-          </BrowserRouter>
+          </HashRouter>
      </StrictMode>,
 );
